e442535386f01cad82fd48d4620a4d15a761cf3a
vikash <vvikash157@gmail.com>
2021-09-02 01:52:27 +0530

    Delete button added

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -4,9 +4,9 @@ import React from "react";
     class App extends React.Component{
 
          state={
-        Tasks:["make Coffee","house cleaning","drink water 10 times","sleep at 10 pm","good use of free time"],
-            currInput="",
-        };
+         Tasks:["make Coffee","house cleaning","drink water 10 times","sleep at 10 pm","good use of free time"],
+         currInput : "",
+        }
 
         render=()=>{
 
@@ -21,20 +21,20 @@ import React from "react";
                    }}
 
                    onKeyDown={(e)=>{
-                       if(e.key=="Enter"){
+                       if(e.key==="Enter"){
                            this.setState({
                                Tasks:[...this.state.currInput,this.state.currInput],
-                               currInput="",
+                               currInput:"",
                            });
                        }
                    }}
 
-                   value={this.this.state.currInput}
+                   value={this.state.currInput}
                    />
                     <ul>
                         {this.state.Tasks.map((el)=>{
 
-                            return <li>{el}</li>
+                            return <li> {el}     <button>Delete</button>    </li>
                         })}
                     </ul>
                 </div>
